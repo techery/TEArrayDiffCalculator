a265868da1939c717863b0f0f8c77d5d29f6d69f
Anastasiya Gorban <gorbannastya@gmail.com>
2015-11-24 12:12:02 +0200

    extend TEArrayDiff+NSIndexPath category to support section number, provide interface documentation

--- Pod/Classes/TEArrayDiff+NSIndexPath.m
+++ Pod/Classes/TEArrayDiff+NSIndexPath.m
@@ -10,42 +10,60 @@
 
 @implementation TEArrayDiff (NSIndexPath)
 
-- (NSArray<NSIndexPath *> *)insertedIndexPaths {
-    return [self transformDiffsToIndexPaths:self.inserted];
+- (NSArray<NSIndexPath *> *)insertedIndexPathsForSection:(NSUInteger)section {
+    return [self transformDiffsToIndexPaths:self.inserted forSection:0];
+}
 
+- (NSArray<NSIndexPath *> *)insertedIndexPaths {
+    return [self insertedIndexPathsForSection:0];
+}
+
+- (NSArray<NSIndexPath *> *)deletedIndexPathsForSection:(NSUInteger)section {
+    return [self transformDiffsToIndexPaths:self.deleted forSection:0];
 }
 
 - (NSArray<NSIndexPath *> *)deletedIndexPaths {
-    return [self transformDiffsToIndexPaths:self.deleted];
+    return [self deletedIndexPathsForSection:0];
+}
+
+- (NSArray<NSIndexPath *> *)updatedIndexPathsForSection:(NSUInteger)section {
+    return [self transformDiffsToIndexPaths:self.updated forSection:section];
 }
 
 - (NSArray<NSIndexPath *> *)updatedIndexPaths {
-    return [self transformDiffsToIndexPaths:self.updated];
+    return [self updatedIndexPathsForSection:0];
 }
 
-- (NSArray<TEMoveIndexPath *> *)movedIndexPaths {
+- (NSArray<TEMoveIndexPath *> *)movedIndexPathsForSection:(NSUInteger)section {
     NSMutableArray *movedIndexPaths = [NSMutableArray arrayWithCapacity:self.moved.count];
     [self.moved enumerateObjectsUsingBlock:^(TEDiffIndex *obj, NSUInteger idx, BOOL *stop) {
-        NSIndexPath *toIndexPath = [NSIndexPath indexPathForItem:obj.index inSection:0];
-        NSIndexPath *fromIndexPath = [NSIndexPath indexPathForItem:obj.fromIndex inSection:0];
+        NSIndexPath *toIndexPath = [NSIndexPath indexPathForItem:obj.index inSection:section];
+        NSIndexPath *fromIndexPath = [NSIndexPath indexPathForItem:obj.fromIndex inSection:section];
         TEMoveIndexPath *moveIndexPath = [TEMoveIndexPath pathWithFromIndexPath:fromIndexPath toIndexPath:toIndexPath];
         [movedIndexPaths addObject:moveIndexPath];
     }];
-
+    
     return [NSArray arrayWithArray:movedIndexPaths];
+}
 
+- (NSArray<TEMoveIndexPath *> *)movedIndexPaths {
+    return [self movedIndexPathsForSection:0];
 }
 
 #pragma mark - Private
 
-- (NSArray<NSIndexPath *> *)transformDiffsToIndexPaths:(NSArray<TEDiffIndex *> *)diffs {
+- (NSArray<NSIndexPath *> *)transformDiffsToIndexPaths:(NSArray<TEDiffIndex *> *)diffs forSection:(NSUInteger)section {
     NSMutableArray *insertedIndexPaths = [NSMutableArray arrayWithCapacity:diffs.count];
     [diffs enumerateObjectsUsingBlock:^(TEDiffIndex *obj, NSUInteger idx, BOOL *stop) {
-        NSIndexPath *indexPath = [NSIndexPath indexPathForItem:obj.index inSection:0];
+        NSIndexPath *indexPath = [NSIndexPath indexPathForItem:obj.index inSection:section];
         [insertedIndexPaths addObject:indexPath];
     }];
-
+    
     return [NSArray arrayWithArray:insertedIndexPaths];
+}
+
+- (NSArray<NSIndexPath *> *)transformDiffsToIndexPaths:(NSArray<TEDiffIndex *> *)diffs {
+    return [self transformDiffsToIndexPaths:diffs forSection:0];
 }
 
 @end
