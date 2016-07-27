41195c386933e30a7e5635ffe8923fb932cbb4bd
Eugene Tulusha <tulusheg@gmail.com>
2016-07-27 16:28:09 +0300

    Add dynamic framework for carthage support

---
+++ Example/Pods/TEArrayDiffCalculatorFramework/TEArrayDiffCalculatorFramework.h
@@ -0,0 +1,19 @@
+//
+//  TEArrayDiffCalculatorFramework.h
+//  TEArrayDiffCalculatorFramework
+//
+//  Created by Eugene on 7/27/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TEArrayDiffCalculatorFramework.
+FOUNDATION_EXPORT double TEArrayDiffCalculatorFrameworkVersionNumber;
+
+//! Project version string for TEArrayDiffCalculatorFramework.
+FOUNDATION_EXPORT const unsigned char TEArrayDiffCalculatorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TEArrayDiffCalculatorFramework/PublicHeader.h>
+
+
